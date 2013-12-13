af106c40f3b13e4044ecc94c6be27bae6d8d6487
marx8926 <artmar89@gmail.com>
2013-12-13 01:13:42 -0500

    guardar diezmo

--- src/AE/ContabilidadBundle/Controller/DiezmoController.php
+++ src/AE/ContabilidadBundle/Controller/DiezmoController.php
@@ -19,12 +19,14 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Security\Core\SecurityContext;
+use AE\DataBundle\Entity\Diezmos;
+use AE\DataBundle\Entity\Persona;
 
 class DiezmoController extends Controller {
     //put your code here
      public function viewAction()
     {
-        
+        return $this->render('AEContabilidadBundle:Default:diezmo.html.twig');
     }
     public function editAction()
     {
@@ -32,7 +34,43 @@ class DiezmoController extends Controller {
     }
     public function saveAction()
     {
+        $request = $this->get('request');
+        $datos =$request->request->get('formulario');        
+        $em = $this->getDoctrine()->getManager();
         
+        $em->beginTransaction();
+        
+        try{
+            $diezmos = new Diezmos();
+            
+            $fechaConv_b = $datos['fecha'];            
+            $fechaConv_a = explode('/', $fechaConv_b, 3);            
+            $fecha = $fechaConv_a[2].'-'.$fechaConv_a[1].'-'.$fechaConv_a[0]; 
+            
+            $diezmos->setDatDiezmoFecharegistro(new \DateTime($fecha));
+            $diezmos->setDecDiezmoMonto($datos['monto']);
+            $diezmos->setVarDiezmoPeticion($datos['peticion']);
+            
+            $persona = NULL;
+            if($datos['persona_id'] != '-1')
+                $persona = $em->getRepository('AEDataBundle:Persona')->find($datos['persona_id']);
+            
+            $diezmos->setPersona($persona);
+            
+            $em->persist($diezmos);
+            
+            $em->flush();
+            
+            $em->commit();
+            $return=array("responseCode"=>200, "greeting"=>"Ok");
+        } catch (Exception $ex) {
+            $em->rollback();
+            $em->close();
+            $em->clear();
+            $return=array("responseCode"=>400, "greeting"=>"Ok");
+        }
+        
+        return new JsonResponse($return); 
     }
     public function infoAction()
     {
