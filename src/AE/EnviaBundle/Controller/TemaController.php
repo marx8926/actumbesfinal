7c8b3dc9944dd528db8ee1d026974bf6a22db65f
marx8926 <artmar89@gmail.com>
2013-12-13 14:26:52 -0500

    registro tema celula sin archivo

--- src/AE/EnviaBundle/Controller/TemaController.php
+++ src/AE/EnviaBundle/Controller/TemaController.php
@@ -20,6 +20,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Security\Core\SecurityContext;
+use AE\DataBundle\Entity\TemaCelula;
 
 
 class TemaController extends Controller {
@@ -32,7 +33,40 @@ class TemaController extends Controller {
     
     public function saveAction()
     {
+        //aun sin guardar archivo
         
+         $request = $this->get('request');
+        $datos =$request->request->get('formulario');
+        $return = array();
+        
+        $em = $this->getDoctrine()->getManager();
+        
+        $em->beginTransaction();
+        
+        try{
+            
+           $tema = new TemaCelula();
+           $tema->setAutor($datos['author']);
+           $tema->setDescripcion($datos['descripcion']);
+           $tema->setTitutlo($datos['titulo']);
+           $tema->setTipo($datos['tipo']);
+           
+           $em->persist($tema);
+           $em->flush();
+            
+            $em->commit();
+            $em->clear();
+            
+            $return=array("responseCode"=>200, "greeting"=>"Ok");
+        } catch (Exception $ex) {
+
+            $em->rollback();
+            $em->clear();
+            $em->close();
+            $return=array("responseCode"=>400, "greeting"=>"Ok");
+        }
+        
+        return new JsonResponse($return);
     }
     
     public function editAction()
