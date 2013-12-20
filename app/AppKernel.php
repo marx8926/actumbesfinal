a438db26ad2f7bf010d4844b1ef772e867510052
marx8926 <artmar89@gmail.com>
2013-12-20 03:10:08 -0500

    pdf bundle ready

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -30,6 +30,7 @@ class AppKernel extends Kernel
             new AE\ConsolidaBundle\AEConsolidaBundle(),
             new AE\DiscipulaBundle\AEDiscipulaBundle(),
             new AE\SecretariaBundle\AESecretariaBundle(),
+            new Ps\PdfBundle\PsPdfBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- src/AE/ToolBundle/Controller/DefaultController.php
+++ src/AE/ToolBundle/Controller/DefaultController.php
@@ -2,12 +2,24 @@
 
 namespace AE\ToolBundle\Controller;
 
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Ps\PdfBundle\Annotation\Pdf;
 class DefaultController extends Controller
 {
+    /**
+    * @Pdf()
+    */
     public function indexAction($name)
     {
-        return $this->render('AEToolBundle:Default:index.html.twig', array('name' => $name));
+       // return $this->render('AEToolBundle:Default:index.html.twig', array('name' => $name));
+        $format = $this->get('request')->get('_format');
+
+        return $this->render(sprintf('AEToolBundle:Default:index.%s.twig', $format), array(
+            'name' => $name,
+            ));
+        
+        
     }
 }
