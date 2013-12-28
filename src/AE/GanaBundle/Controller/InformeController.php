a1f52b5f65cdc1f7ac3f17f7a5415d25f9451151
marx8926 <artmar89@gmail.com>
2013-12-28 12:52:08 -0500

    reporte en excel con formato

--- src/AE/GanaBundle/Controller/InformeController.php
+++ src/AE/GanaBundle/Controller/InformeController.php
@@ -72,23 +72,12 @@ class InformeController extends Controller {
     }
     
     public function detallado_serviceAction()
-    {
-            // ask the service for a Excel5
-        
+    {        
         $request = $this->get('request');
         $red =$request->request->get('red_lista');
         $desde = $request->request->get('desde');
         $hasta = $request->request->get('hasta');
         $em = $this->getDoctrine()->getManager();
-        
-        
-       $phpExcelObject = $this->get('phpexcel')->createPHPExcelObject();
-
-       $phpExcelObject->getProperties()->setCreator("AdminChurch.com")
-           ->setLastModifiedBy("AdminChurch.com")
-           ->setTitle("AdminChurch.com 2005 XLSX Document")
-           ->setSubject("AdminChurch.com 2005 XLSX  Document")
-           ->setCategory("AdminChurch.com report");
        
        $todos = array();
        
@@ -100,6 +89,7 @@ class InformeController extends Controller {
        $fech_a =explode('/', $fech_b,3);
        $fin = $fech_a[2].'-'.$fech_a[1].'-'.$fech_a[0]; 
        
+       
        if($red != -1)
        {
            if($red == 0)
@@ -107,8 +97,7 @@ class InformeController extends Controller {
                $sql = "select * from get_informe_todos_rango(:ini,:fin)";
                $smt = $em->getConnection()->prepare($sql);
                $smt->execute(array(':ini'=>$inicio, ':fin'=>$fin));
-               $todos = $smt->fetchAll();
-               
+               $todos = $smt->fetchAll();               
            }
            else
            {
@@ -118,31 +107,49 @@ class InformeController extends Controller {
                 $todos = $smt->fetchAll();
            }
        }
+       getcwd();
+       chdir('report\ganar');
+       $path = getcwd()."\informe_detallado.xls";
+       $objReader = $this->get('phpexcel')->createPHPExcelObject($path);
        
-       $phpExcelObject->setActiveSheetIndex(0)
-               ->setCellValue('A1', 'Semana del '.$inicio.' al '.$fin)
-           ->setCellValue('A2', 'RED')
-               ->setCellValue('B2', 'DOCE DEL PASTOR')
-               ->setCellValue('C2','NOMBRES Y APELLIDOS DEL NUEVO')
-               ->setCellValue('D2','CONVERSION')
-               ->setCellValue('E2','LIDER GANADRO DE ALMAS')
-               ->setCellValue('F2','¿DONDE LO GANÓ?');
+       $objReader->setActiveSheetIndex(0)
+           ->setCellValue('F5', $inicio)
+           ->setCellValue('F6', $fin);
        
-       $phpExcelObject->getActiveSheet()->setTitle('Ganados');
+       $objReader->getActiveSheet()->getColumnDimension('A')->setAutoSize('true');
+       $objReader->getActiveSheet()->getColumnDimension('B')->setAutoSize('true');
+       $objReader->getActiveSheet()->getColumnDimension('C')->setAutoSize('true');
+       $objReader->getActiveSheet()->getColumnDimension('D')->setAutoSize('true');
+       $objReader->getActiveSheet()->getColumnDimension('E')->setAutoSize('true');
+       $objReader->getActiveSheet()->getColumnDimension('F')->setAutoSize('true');
        
-       
-       $phpExcelObject->getActiveSheet()->fromArray($todos, NULL, 'A3');
-      
-       // Set active sheet index to the first sheet, so Excel opens this as the first sheet
-       $phpExcelObject->setActiveSheetIndex(0);
-
+       /*
+       $head = array(
+            'borders' => array(
+                'allborders' => array(
+                    'style' => 'double',            
+                ),
+            ),
+        );       
+        $body = array(
+            'borders' => array(
+                'allborders' => array(
+                    'style' => 'dotted',            
+                ),
+            ),
+        );         
+         $phpExcelObject->getActiveSheet()->getStyle('A2:F2')->applyFromArray($head);
+         $phpExcelObject->getActiveSheet()->getStyle('A3:F3')->applyFromArray($body);       
+       */
+       $objReader->getActiveSheet()->fromArray($todos, NULL, 'A9');
+     
         // create the writer
-        $writer = $this->get('phpexcel')->createWriter($phpExcelObject, 'Excel5');
+        $writer = $this->get('phpexcel')->createWriter($objReader, 'Excel2007');
         // create the response
         $response = $this->get('phpexcel')->createStreamedResponse($writer);
         // adding headers
         $response->headers->set('Content-Type', 'text/vnd.ms-excel; charset=utf-8');
-        $response->headers->set('Content-Disposition', 'attachment;filename=reporte-'.$inicio.'_'.$fin.'.xls');
+        $response->headers->set('Content-Disposition', 'attachment;filename=reporte.xlsx');
         $response->headers->set('Pragma', 'public');
         $response->headers->set('Cache-Control', 'maxage=1');
 
