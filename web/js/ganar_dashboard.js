c5e2ad5b9749bee188023a4ce0153814b51646f0
marx8926 <artmar89@gmail.com>
2013-12-28 16:37:59 -0500

    dashboard 10%

---
+++ web/js/ganar_dashboard.js
@@ -0,0 +1,61 @@
+meses = [
+        {
+           "id" : "1" , "mes": "Enero"            
+        },
+        {
+           "id" : "2" , "mes": "Febrero"            
+        },
+        {
+           "id" : "3" , "mes": "Marzo"            
+        },
+        {
+           "id" : "1" , "mes": "Enero"            
+        },
+        {
+           "id" : "2" , "mes": "Febrero"            
+        },
+        {
+           "id" : "3" , "mes": "Marzo"            
+        },
+        {
+           "id" : "4" , "mes": "Abril"            
+        },
+        {
+           "id" : "5" , "mes": "Mayo"            
+        },
+        {
+           "id" : "6" , "mes": "Junio"            
+        },
+        {
+           "id" : "7" , "mes": "Julio"            
+        },
+        {
+           "id" : "8" , "mes": "Agosto"            
+        },
+        {
+           "id" : "9" , "mes": "Septiembre"
+        },
+        {
+           "id" : "10" , "mes": "Octubre"            
+        },
+        {
+           "id" : "11" , "mes": "Noviembre"            
+        },
+        {
+           "id" : "12" , "mes": "Diciembre"            
+        }
+];
+       
+trimestres =[
+        {
+            "id": "1", "mes": "Enero-Marzo"},
+        {
+            "id": "2", "mes": "Abril-Junio"},
+        {
+            "id":"3", "mes": "Julio-Septiembre"},
+        {
+            "id":"4", "mes": "Octubre-Diciembre"
+        }
+    ];
+
+
