410730960cb6daaca3a81090a31f6eb700bc6b1f
marx8926 <artmar89@gmail.com>
2013-12-30 14:23:57 -0500

    docentes y estudiantes

--- src/AE/DiscipulaBundle/Controller/InformeController.php
+++ src/AE/DiscipulaBundle/Controller/InformeController.php
@@ -43,4 +43,72 @@ class InformeController extends Controller {
     {
         
     }
+    
+    public function informe_estudiantes_redAction()
+    {
+
+        $request = $this->get('request');
+        $red =$request->request->get('net');
+        
+       getcwd();
+       chdir('report\discipular');
+       $path = getcwd()."\informe_estudiantes.xls";
+       
+       $phpExcelObject = $this->get('phpexcel')->createPHPExcelObject($path);
+
+       $sql = "select * from get_niveles_all_tabla_red_exportar(:net,4,1)";
+       $em = $this->getDoctrine()->getManager();
+       $smt = $em->getConnection()->prepare($sql);
+       $smt->execute(array(':net'=>$red));
+       $todos = $smt->fetchAll();
+       
+       $phpExcelObject->getActiveSheet()->fromArray($todos, NULL, 'A9');
+       // Set active sheet index to the first sheet, so Excel opens this as the first sheet
+       $phpExcelObject->setActiveSheetIndex(0);
+
+        // create the writer
+        $writer = $this->get('phpexcel')->createWriter($phpExcelObject, 'Excel2007');
+        // create the response
+        $response = $this->get('phpexcel')->createStreamedResponse($writer);
+        // adding headers
+        $response->headers->set('Content-Type', 'text/vnd.ms-excel; charset=utf-8');
+        $response->headers->set('Content-Disposition', 'attachment;filename=estudiantes.xlsx');
+        $response->headers->set('Pragma', 'public');
+        $response->headers->set('Cache-Control', 'maxage=1');
+
+        return $response; 
+    }
+
+    
+    public function informe_docentes_allAction()
+    {        
+       
+       getcwd();
+       chdir('report\discipular');
+       $path = getcwd()."\informe_docentes.xls";
+       
+       $phpExcelObject = $this->get('phpexcel')->createPHPExcelObject($path);
+
+       $sql = "SELECT  nombres, red, inicio, telefonos, estado FROM view_get_profesores_all";
+       $em = $this->getDoctrine()->getManager();
+       $smt = $em->getConnection()->prepare($sql);
+       $smt->execute();
+       $todos = $smt->fetchAll();
+       
+       $phpExcelObject->getActiveSheet()->fromArray($todos, NULL, 'A9');
+       // Set active sheet index to the first sheet, so Excel opens this as the first sheet
+       $phpExcelObject->setActiveSheetIndex(0);
+
+        // create the writer
+        $writer = $this->get('phpexcel')->createWriter($phpExcelObject, 'Excel2007');
+        // create the response
+        $response = $this->get('phpexcel')->createStreamedResponse($writer);
+        // adding headers
+        $response->headers->set('Content-Type', 'text/vnd.ms-excel; charset=utf-8');
+        $response->headers->set('Content-Disposition', 'attachment;filename=docentes.xlsx');
+        $response->headers->set('Pragma', 'public');
+        $response->headers->set('Cache-Control', 'maxage=1');
+
+        return $response; 
+    }
 }
