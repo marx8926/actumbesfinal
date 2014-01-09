c5d1de0dd0799063af9dbead9ceec85181c5c4fe
marx8926 <artmar89@gmail.com>
2014-01-09 10:09:16 -0500

    secretario servicios eliminado

--- src/AE/ServiciosBundle/Controller/SecretariaServicioController.php
+++ src/AE/ServiciosBundle/Controller/SecretariaServicioController.php
@@ -20,36 +20,5 @@ use Symfony\Component\Serializer\Encoder\JsonEncoder;
  * @author Marks-Calderon
  */
 class SecretariaServicioController extends Controller{
-<<<<<<< HEAD
-    
-=======
-    //put your code here
-    public function bautizo_allAction()
-    {
-        $em = $this->getDoctrine()->getEntityManager();
-	
-		$sql = "select * from view_get_all_bautizo";
-	
-		$smt = $em->getConnection()->prepare($sql);
-		$smt->execute();
-	
-		$todo = $smt->fetchAll();
-	                
-		return new JsonResponse(array('aaData'=>$todo));
-    }
 
-    public function encuentros_allAction()
-    {
-        $em = $this->getDoctrine()->getEntityManager();
-	
-		$sql = "select * from view_get_all_encuentro;";
-	
-		$smt = $em->getConnection()->prepare($sql);
-		$smt->execute();
-	
-		$todo = $smt->fetchAll();
-	                
-		return new JsonResponse(array('aaData'=>$todo));
-    }
->>>>>>> a4a04111d020649161e61278be3ce626d677fa6a
 }
